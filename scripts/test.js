d3fa9f574e874885b7786003d5ae6fd37e6b9c3c
zhanglinxiao <lingxiao.zhang@zozo.cn>
2021-12-24 19:11:59 +0800

    Mac_AutoSave

---
+++ scripts/test.js
@@ -0,0 +1,11 @@
+// With a named function
+let myGreeting = setTimeout(function sayHi() {
+  console.log('Hello, Mr. Universe!');
+}, 2000);
+
+// With a function defined separately
+// function sayHi() {
+//   alert('Hello Mr. Universe!');
+// };
+
+// let myGreeting = setTimeout(sayHi, 2000);
